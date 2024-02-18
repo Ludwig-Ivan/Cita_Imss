97a001f309f4b87662fb33b96ec79be914bba069
Ludwig Ivan <ivansierras11@gmail.com>
2024-02-18 13:17:32 -0600

    Primer Commit

---
+++ src/components/Formulario.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Modal, Text } from 'react-native'
+
+const Formulario = () => {
+  return (
+    <Modal>
+        <Text>
+            Hola
+        </Text>
+    </Modal>
+  )
+}
+
+export default Formulario
